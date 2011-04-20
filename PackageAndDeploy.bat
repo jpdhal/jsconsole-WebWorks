486dcc6a47d2f15602b5ab1cc142a24ad8a2d3d0
Muerl <haagmm@gmail.com>
2011-04-19 22:34:32 -0400

    Added Signing / Not Signing Logic

--- PackageAndDeploy.bat
+++ PackageAndDeploy.bat
@@ -5,20 +5,27 @@ REM Replace your IP/Name/Password/InstallDir for mine.
 REM this depends on info-zip which is available for MOST platforms here : http://www.info-zip.org/FAQ.html#download
 
 
-SET ip=192.168.1.129
+SET ip=192.168.1.6
 SET name=JSConsole
 SET password=a
-SET installDir="C:\rim\playbook\BlackBerry WebWorks Packager for PlayBook\bbwp"
+SET installDir="C:\rim\Playbook\SDK2.0\bbwp"
 SET old_Java_home=%JAVA_HOME%
 SET JAVA_HOME=%installDir%\..\jre\bin
 SET mycskpassword=
 SET myp12password=
-set mybuildId=15
+set mybuildId=22
 
 del %name%.zip %name%.bar %name%.bar.sig
 
 zip -0 -r %name%.zip * -x .* *.exe *.bar *.zip *.bat
-%installDir%\bbwp %name%.zip -gcsk %mycskpassword% -gp12 %myp12password% -buildId %mybuildId% -o ./
+
+if X==X%mycskpassword% GOTO unsignedbuild
+	%installDir%\bbwp %name%.zip -gcsk %mycskpassword% -gp12 %myp12password% -buildId %mybuildId% -o ./
+	goto finish
+	:unsignedbuild
+	%installDir%\bbwp %name%.zip -o ./ 
+	goto finish
+:finish
 %installDir%\blackberry-tablet-sdk\bin\blackberry-deploy   -installApp -password %password% -device %ip% -devMode -package %name%.bar
 
 SET JAVA_HOME=%old_Java_home%
